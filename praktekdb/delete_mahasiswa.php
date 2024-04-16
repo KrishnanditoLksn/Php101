3c6e145471f9d3eb87774914363b723b14ace808
KrishnanditoLksn <krislaksana123@gmail.com>
2024-04-16 19:35:13 +0700

    feat : delete validation

--- praktekdb/delete_mahasiswa.php
+++ praktekdb/delete_mahasiswa.php
@@ -3,3 +3,8 @@ $conn = include("config.php");
 $id = $_GET["id"];
 $mhs_delete= "DELETE FROM mahasiswa WHERE id = $id";
 $delete_query = mysqli_query($conn,$mhs_delete);
+
+if (isset($delete_query)) {
+    # code...
+    echo "sukses dihapus";
+}
