7bc2aa179d9d8a689a389dace9f549f208ec7dd7
KrishnanditoLksn <krislaksana123@gmail.com>
2024-04-28 06:36:31 +0700

    feat : this and encapsulation

--- oop/class/Animal.php
+++ oop/class/Animal.php
@@ -5,14 +5,51 @@ class Animal
     public  $animal_name;
     public $animal_sex;
 
+    private $animal_age;
+
+    public function setAnimalAge($age)
+    {
+        $animal_age = trim($age);
+        if ($animal_age == ' ') {
+            return "Empty name !!";
+        }
+        $this->animal_age = $age;
+    }
+
+    public function getAnimalAge()
+    {
+        return $this->animal_age;
+    }
+
     public function getAnimalName()
     {
         return $this->animal_name;
+    }
+
+    public function getAnimalSex()
+    {
+        return $this->animal_sex;
+    }
+
+
+    public function isAnimalOld()
+    {
+        if ($this->getAnimalAge() > 5) {
+            echo $this->getAnimalName(), "Old animal";
+        } else {
+            $this->animal_age += 1;
+        }
     }
 }
 
 //create new object 
 $animal = new Animal();
-$animal->$animal_name = "Anima";
-$animal->getAnimalName();
-var_dump($animal);
+//set a property
+$animal->animal_name = "Anima";
+$animal->animal_sex = "Male";
+echo ($animal->getAnimalName());
+echo $animal->getAnimalSex();
+$animal->setAnimalAge(23);
+echo "Age : ", $animal->getAnimalAge();
+
+$animal->isAnimalOld();
--- oop/class/Fox.php
+++ oop/class/Fox.php
@@ -1,4 +1,4 @@
-<?
+<?php
 class Fox extends Animal{
     
 }
